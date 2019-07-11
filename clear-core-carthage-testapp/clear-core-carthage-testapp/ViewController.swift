29b8e83aea27e07d819d4c9dbbe9a7f41070f4e2
Gabriel Siu <gabriel.siu@clearbridgemobile.com>
2019-07-11 10:54:13 -0400

    Add initial base project

---
+++ clear-core-carthage-testapp/clear-core-carthage-testapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clear-core-carthage-testapp
+//
+//  Created by Gabriel Siu on 2019-07-11.
+//  Copyright © 2019 Clearbridge Mobile Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
